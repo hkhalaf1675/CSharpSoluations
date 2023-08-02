6a3f0813b1cd8825ecf53fbc9dbe997278d18b85
hkhalaf1675 <58273369+hkhalaf1675@users.noreply.github.com>
2023-08-03 00:59:09 +0300

    Add files via upload

---
+++ Assuit_MInya_contest6/Assuit_MInya_contest6/Program.cs
@@ -0,0 +1,281 @@
+﻿//// Palindrome
+
+//using System.ComponentModel;
+
+//static bool palindrome(long num)
+//{
+//    long temp = num;
+//    long pali = 0;
+//    while (temp != 0)
+//    {
+//        pali = (pali * 10) + (temp % 10);
+//        temp = temp / 10;
+//    }
+//    if (pali == num)
+//        return true;
+//    else
+//        return false;
+//}
+
+//int t = int.Parse(Console.ReadLine());
+//for (int y = 0; y < t; y++)
+//{
+//    long num = long.Parse(Console.ReadLine());
+//    {
+//        long i = 10000000;
+//        bool f = false;
+//        if (num % 10 == 0)
+//            f = false;
+//        else
+//        {
+//            for (i = i ; i < Math.Pow(10, 9); i++)
+//            {
+//                if (i % num == 0)
+//                {
+//                    if (palindrome(i))
+//                    {
+//                        Console.WriteLine(i);
+//                        f = true;
+//                        break;
+//                    }
+//                    else
+//                    {
+
+//                    }
+//                }
+//            }
+//        }
+//        if (f == false)
+//            Console.WriteLine(-1);
+//    }
+//}
+
+// ====================================================>
+
+// Problem C
+
+//int t = int.Parse(Console.ReadLine());
+//for(int y=0; y<t; y++)
+//{
+//    long[] values = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+//    long tasty_cake = 0;
+
+//    if (values[2] > values[1])
+//    {
+//        values[2] = values[2] - values[1];
+//        tasty_cake += values[1] * 2;
+
+//        if (values[2] > values[0])
+//        {
+//            tasty_cake += values[0] + 1;
+//        }
+//        else
+//        {
+//            tasty_cake += values[2];
+//        }
+//    }
+//    else
+//    {
+//        tasty_cake = values[1] + values[2];
+//    }
+//    Console.WriteLine(tasty_cake);
+//}
+
+// ================================================>
+
+// Problem E
+
+//int t = int.Parse(Console.ReadLine());
+//for (int y = 0; y < t; y++)
+//{
+//    long num = long.Parse(Console.ReadLine());
+//    long[] numbers = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+//    long two_divisor = 0;
+//    long three_divisor = 0;
+
+//    foreach (long x in numbers)
+//    {
+//        int divisor = 2;
+//        if (x == 1 || x == 0)
+//            divisor = 1;
+
+//        for (long i = 2; i < x; i++)
+//        {
+//            if (x % i == 0)
+//                divisor++;
+//            if (divisor > 3)
+//                break;
+//        }
+//        if (divisor == 2)
+//            two_divisor++;
+//        else if (divisor == 3)
+//            three_divisor++;
+//    }
+//    long min = Math.Abs(two_divisor - three_divisor);
+//    if (min == 0)
+//        min = 1;
+
+//    long pair_count = 1;
+
+//    for (long i = min; i <= Math.Max(two_divisor, three_divisor); i++)
+//        pair_count = pair_count * i;
+
+//    if (two_divisor == 0 || three_divisor == 0)
+//        pair_count = 0;
+
+//    Console.WriteLine(pair_count);
+//}
+
+// ========================================================================>
+
+// Round 881
+// Problem A
+//int t = int.Parse(Console.ReadLine());
+//for (int y=0; y < t; y++)
+//{
+//    int n = int.Parse(Console.ReadLine());
+//    int[] numbers=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+//    Array.Sort(numbers);
+
+//    int colors = n / 2;
+//    int cost = 0;
+//    int head = 0;
+//    int tail = n - 1;
+
+//    while (colors > 0)
+//    {
+//        cost += numbers[tail] - numbers[head];
+//        head++;
+//        tail--;
+//        colors--;
+//    }
+
+//    Console.WriteLine(cost);
+//}
+
+// =======================================================>
+
+// Problem c
+//int t = int.Parse(Console.ReadLine());
+//for(int y=0; y<t; y++)
+//{
+//    long num = long.Parse(Console.ReadLine());
+//    long sum = 0;
+//    while(num > 0)
+//    {
+//        sum += num;
+//        if (num % 2 == 0)
+//            num /= 2;
+//        else
+//            num = (num - 1) / 2;
+//    }
+//    Console.WriteLine(sum);
+//}
+
+// ===========================================================>>
+
+// Problem F
+
+//int t = int.Parse(Console.ReadLine());
+//for(int y=0; y<t; y++)
+//{
+//    long[] values = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+//    long pair_count = 0;
+
+//    for(long i=1; i <= values[0]; i++)
+//    {
+//        for(long j=1; j <= values[1]; j++)
+//        {
+//            if ((i + j) % values[2] == 0)
+//                pair_count++;
+//        }
+//    }
+//    Console.WriteLine(pair_count);
+//}
+
+// ====================================================>
+
+// Problem G
+
+//using System.Numerics;
+
+//int t = int.Parse(Console.ReadLine());
+
+//for (int y = 0; y < t; y++)
+//{
+//    string str = Console.ReadLine();
+
+//    BigInteger num = 0;
+//    long repeat_count = 0;
+
+//    for(int i = 0; i<str.Length; i++)
+//    {
+//        repeat_count = str.Count(n => n == str[i]);
+
+//        if (repeat_count < 10)
+//            num = (num * 10) + repeat_count;
+//        else if (repeat_count < 100 && repeat_count >= 10)
+//            num = (num * 100) + repeat_count;
+//        else if (repeat_count < 1000 && repeat_count >= 100)
+//            num = (num * 1000) + repeat_count;
+//        else
+//            num = (num * 10000) + repeat_count;
+//    }
+
+//    if (palindrome(num))
+//        Console.WriteLine("YES");
+//    else
+//        Console.WriteLine("NO");
+//}
+
+//static bool palindrome(BigInteger num)
+//{
+//    BigInteger temp = num;
+//    BigInteger pali = 0;
+//    while (temp != 0)
+//    {
+//        pali = (pali * 10) + (temp % 10);
+//        temp = temp / 10;
+//    }
+//    if (pali == num)
+//        return true;
+//    else
+//        return false;
+//}
+
+// =================================================>
+
+//PProblem I
+
+int t = int.Parse(Console.ReadLine());
+for(int y=0; y<t; y++)
+{
+    int num = int.Parse(Console.ReadLine());
+    long[] arr = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+    bool f = false;
+    for(int i =0; i<num; i++)
+    {
+        for(int j=i+1; j<num; j++)
+        {
+            for(int k=j+1; k<num; k++)
+            {
+                if ((arr[i] + arr[j] + arr[k]) % 2 == 0)
+                {
+                    Console.WriteLine("Yes");
+                    f = true;
+                    break;
+                }
+                if (f == true)
+                    break;
+            }
+            if (f == true)
+                break;
+        }
+    }
+    if (f == false)
+        Console.WriteLine("NO");
+}
---
+++ Assuit_Minya_contest8/Assuit_Minya_contest8/Program.cs
@@ -0,0 +1,109 @@
+﻿
+//// Problem B
+
+//int num = int.Parse(Console.ReadLine());
+//int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+//Array.Sort(arr);
+
+//long firstnum = 0;
+//long secondnum = 0;
+
+//if (arr[0] == 0)
+//{
+//    arr[0] = arr[1];
+//    arr[1] = 0;
+//}
+//if (arr[num / 2] == 0)
+//{
+//    arr[num / 2] = arr[(num / 2) + 1];
+//    arr[(num / 2) + 1] = 0;
+//}
+
+//for(int i = 0; i < num; i++)
+//{
+//    if(i < num / 2)
+//    {
+//        firstnum = (firstnum * 10) + arr[i];
+//    }
+//    else
+//        secondnum=(secondnum * 10) + arr[i];
+//}
+
+//secondnum = reverse(secondnum);
+//Console.WriteLine(firstnum + " " + secondnum);
+//Console.WriteLine(secondnum - firstnum);
+
+//static long reverse(long num)
+//{
+//    long temp = 0;
+//    while (num > 0)
+//    {
+//        temp = (temp * 10) + (num % 10);
+//        num /= 10;
+//    }
+//    return temp;
+//}
+
+// ======================================================================>
+
+// Problem D
+
+//int num = int.Parse(Console.ReadLine());
+
+//List<long> numbers = new List<long>();
+
+//for(int y=0; y<num; y++)
+//{
+//    long[] range = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+//    numbers.AddRange(range);
+//}
+
+//int count = 0;
+
+//for(int i=0; i<numbers.Count; i++)
+//{
+//    for(int j=i+1; j<numbers.Count; j++)
+//    {
+//        for(int k=j+1; k<numbers.Count; k++)
+//        {
+//            for(int y=k+1; y<numbers.Count; y++)
+//            {
+//                if (i + j + k + y == 0)
+//                    count++;
+//            }
+//        }
+//    }
+//}
+//Console.WriteLine(count);
+
+// ==========================================================>
+
+//// Problem E
+
+//long[] values = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+//int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+//for(long y=0; y < values[1]; y++)
+//{
+//    long sum = 0;
+//    long[] index = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+//    for (long i = index[0]-1; i < index[1]; i++)
+//        sum += numbers[i];
+
+//    Console.WriteLine(sum);
+//}
+
+// ==============================================================>
+
+//// Problem F
+
+//string str = Console.ReadLine();
+//double timeA = double.Parse(str.Split(' ')[0]);
+//double timeB = double.Parse(str.Split(' ')[1]);
+//double timeC = double.Parse(str.Split(' ')[2]);
+
+//double time = (timeA * timeB * timeC) / (((timeA + timeB) * timeC) + (timeA * timeB));
+
+//Console.WriteLine(time.ToString("F9"));
+
+//======================================================>
