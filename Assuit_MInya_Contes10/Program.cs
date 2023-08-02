461f75f8217ed4d79ae97819152999fe31f90050
hkhalaf1675 <58273369+hkhalaf1675@users.noreply.github.com>
2023-08-03 01:00:19 +0300

    Add files via upload

---
+++ Assuit_MInya_Contes10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Problem A
+string strNum1 = Console.ReadLine();
+string strNum2 = Console.ReadLine();
+
+char[] str1CharArr = strNum1.ToArray();
+Array.Sort(str1CharArr);
+
+char[] str2CharArr = strNum2.ToArray();
+Array.Sort(str2CharArr);
+
+bool check = true;
+
+for(int i=0; i<str1CharArr.Length; i++)
+{
+    if (str1CharArr[i] != str2CharArr[i])
+    {
+        check = false;
+        break;
+    }
+}
+if(!check || str1CharArr.Length != str2CharArr.Length)
+    Console.WriteLine("NO");
+else
+    Console.WriteLine("YES");
+// ======================================================================<><><><><>
+
---
+++ Assuit_Minya_Contest9/Program.cs
@@ -0,0 +1,45 @@
+﻿//// Probem B
+
+//int t = int.Parse(Console.ReadLine());
+//for(int y =0; y<t; y++)
+//{
+//    string str = Console.ReadLine();
+
+//    long numM = Convert.ToInt64(str.Split(' ')[0]);
+//    long numN = Convert.ToInt64(str.Split(' ')[1]);
+
+//    int counter = 0;
+
+//    for (numM = numM; numM > 0; numM = numM - numN)
+//        counter++;
+
+//    Console.WriteLine(counter);
+//}
+
+// ====================================================>
+
+//// Problem C
+
+//int t = Convert.ToInt32(Console.ReadLine());
+//for(int y=0; y<t; y++)
+//{
+//    long num = Convert.ToInt64(Console.ReadLine());
+
+//    char[] chars = Console.ReadLine().ToCharArray();
+//    int counter = 0;
+//    foreach(char c in chars)
+//    {
+//        if (c == 'A' || c == 'B')
+//            counter++;
+//    }
+//    Console.WriteLine(counter);
+//}
+
+// =========================================>
+
+//Problem E
+string str = Console.ReadLine();
+long x = Convert.ToInt64(str.Split(' ')[0]);
+long y = Convert.ToInt64(str.Split(' ')[1]);
+
+Console.WriteLine(x+(y/2));
